da41d9bce53524f7c49f31eadd24de8df72f3d03
whyleonardo <pleasefloyd73@gmail.com>
2022-07-13 16:18:02 -0300

    desafio ignite feito com vite

---
+++ src/components/MovieCard/index.tsx
@@ -0,0 +1,33 @@
+import { Star, Clock } from 'react-feather'
+
+import './index.scss'
+
+interface MovieCardProps {
+  title: string
+  poster: string
+  rating: string
+  runtime: string
+}
+
+export const MovieCard = (props: MovieCardProps) => {
+  return (
+    <div className="movie-card">
+      <img src={props.poster} alt={props.title} />
+
+      <div>
+        <div className="movie-info">
+          <span>{props.title}</span>
+          <div className="meta">
+            <div>
+              <Star /> {props.rating}
+            </div>
+
+            <div>
+              <Clock /> {props.runtime}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
